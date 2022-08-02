fdc6e49dd126d396e13bd32feb676b80d5d40240
scottcbx <scottcbx@outlook.com>
2022-08-02 16:43:34 -0400

    blog backend

---
+++ config/db/dbConnect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URL, 
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("DB is connected successfully");
+    } catch (error) {
+        console.log(`Error ${error.message}`);
+    }
+};
+
+module.exports = dbConnect;
